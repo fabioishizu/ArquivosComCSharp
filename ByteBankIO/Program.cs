4f92c5505a0e90d064e453f261b39495e4e83c70
Fabio <f.masaoishizu@gmail.com>
2023-01-17 17:53:04 -0300

    [dev] Leitura do buffer no console

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -5,10 +5,26 @@ class Program
     static void Main(string[] args)
     {
         var enderecoDoArquivo = "contas.txt";
+        var numeroDeBytesLidos = -1;
         var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
 
         var buffer = new byte[1024];
-        fluxoDoArquivo.Read(buffer, 0, 1024); // buffer, offset (indica a posição onde irá começar a escrita), índice limte de escrita.
+
+        while(numeroDeBytesLidos != 0)
+        {
+            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024); // buffer, offset (indica a posição onde irá começar a escrita), índice limte de escrita.
+            EscreverBuffer(buffer);
+        }
+
         Console.ReadLine();
+    }
+
+    static void EscreverBuffer(byte[] buffer)
+    {
+        foreach (var meuByte in buffer)
+        {
+            Console.Write(meuByte);
+            Console.Write(" ");
+        }
     }
 }
