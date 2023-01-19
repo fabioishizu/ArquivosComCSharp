c443eca95e8ada2af0e19d51fa300c72e0c73cdb
fabioishizu <f.masaoishizu@gmail.com>
2023-01-19 15:29:35 -0300

    [dev] Refatoração de código, quebrando a program em patial classes.

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,50 +1,10 @@
 ﻿using ByteBankIO;
 using System.Text;
 
-class Program
+partial class Program
 {
     static void Main(string[] args)
     {
-        try
-        {
-            var enderecoDoArquivo = "contas.txt";
-            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
-            {
-                var numeroDeBytesLidos = -1;
-
-                var buffer = new byte[1024];
-
-                while (numeroDeBytesLidos != 0)
-                {
-                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024); // buffer, offset (indica a posição onde irá começar a escrita), índice limte de escrita.
-                    Console.WriteLine($"Bytes lidos: {numeroDeBytesLidos}");
-                    EscreverBuffer(buffer, numeroDeBytesLidos);
-                }
-
-                fluxoDoArquivo.Close();
-                Console.ReadLine();
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-    }
-
-    static void EscreverBuffer(byte[] buffer, int byteslidos)
-    {
-        var utf8 = new UTF8Encoding();
-        var texto = utf8.GetString(buffer,0, byteslidos);
-
-        
-        Console.Write(texto);
-
-        /*
-        foreach (var meuByte in buffer)
-        {
-            Console.Write(meuByte);
-            Console.Write(" ");
-        }
-        */
+        Console.ReadLine();
     }
 }
